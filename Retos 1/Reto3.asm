0cb5fa50b400462cc45b2fd058317b3a52276003
Miguel Angel Restrepo Acevedo <miguel13restrepo@gmail.com>
2025-01-27 17:26:29 -0500

    Retos 4 y 5, Mejor visualizacion de los comentarios

--- Retos 1/Reto3.asm
+++ Retos 1/Reto3.asm
@@ -1,7 +1,10 @@
 //Para mejor visualizasion de este reto, vamos a llenar la posición 24 con un numero de ante mano
+
 @24
 M=1
+
 //ahora hacemos el reto 
+
 @24
 D=M 
 @=200
---
+++ Retos 1/Reto5.asm
@@ -0,0 +1,15 @@
+//Agregamos valores en @0 y @1 previamente
+
+@5
+D=A
+@0
+M=D 
+@1
+M=D-1
+
+//Comenzamos el reto
+
+@0
+D=M 
+@1
+D=D+M
